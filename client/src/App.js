62d6bee5c9fc0509ca5aa5744d5ae2d010579274
pramuditha <pramuditha1@live.com>
2022-05-14 02:41:37 +0530

    Initialize frontend in react, install libraries

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+
+const App = () => {
+    return(
+        <div>
+            <Button variant="contained">todo application</Button>
+        </div>
+    )
+}
+
+export default App
